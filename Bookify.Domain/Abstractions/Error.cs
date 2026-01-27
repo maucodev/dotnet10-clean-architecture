5425710efdea467103ab682c506de2624ecb2ce6
maucodev <hola@maucodev.com>
2026-01-26 21:52:39 -0600

    Add domain events

---
+++ Bookify.Domain/Abstractions/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Domain.Abstractions;
+
+public record Error(string Code, string Name)
+{
+    public static Error None = new(Code: string.Empty, Name: string.Empty);
+
+    public static Error NullValue = new(Code: "Error.NullValue", Name: "Null value was provided");
+}
---
+++ Bookify.Domain/Bookings/BookingErrors.cs
@@ -0,0 +1,26 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Bookings;
+
+public static class BookingErrors
+{
+    public static Error NotFound = new(
+        Code: "Booking.NotFound",
+        Name: "The booking with the specified identifier was not found");
+
+    public static Error Overlap = new(
+        Code: "Booking.Overlap",
+        Name: "The current booking is overlapping with an existing one");
+
+    public static Error NotReserved = new(
+        Code: "Booking.NotReserved",
+        Name: "The booking is not reserved");
+
+    public static Error NotConfirmed = new(
+        Code: "Booking.NotConfirmed",
+        Name: "The booking is not confirmed");
+
+    public static Error AlreadyStarted = new(
+        Code: "Booking.AlreadyStarted",
+        Name: "The booking has already started");
+}
