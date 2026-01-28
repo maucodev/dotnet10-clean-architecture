8842b51e42426ec9b49492fd6feafdcc4f466253
maucodev <hola@maucodev.com>
2026-01-27 21:48:17 -0600

    Implementing Queries with Dapper for blazing performance

---
+++ Bookify.Application/Apartments/SearchApartment/SearchApartmentQuery.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using Bookify.Application.Abstractions.Messaging;
+
+namespace Bookify.Application.Apartments.SearchApartment;
+
+public sealed record SearchApartmentQuery(
+    DateOnly StartDate,
+    DateOnly EndDate) : IQuery<IReadOnlyList<ApartmentResponse>>;
