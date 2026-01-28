ffd61e54985b71df015c252c3e9bb6d610df301a
maucodev <hola@maucodev.com>
2026-01-27 21:08:34 -0600

    Defining a domain event handler

---
+++ Bookify.Application/Abstractions/Email/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Bookify.Application.Abstractions.Email;
+
+public interface IEmailService
+{
+    Task SendAsync(
+        Domain.Users.Email recipient, 
+        string subject, 
+        string body);
+}
---
+++ Bookify.Application/Bookings/ReserveBooking/BookingReservedDomainEventHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Bookify.Application.Abstractions.Email;
+using Bookify.Domain.Bookings.Events;
+using Bookify.Domain.Bookings.Repository;
+using Bookify.Domain.Users.Repository;
+using MediatR;
+
+namespace Bookify.Application.Bookings.ReserveBooking;
+
+internal sealed class BookingReservedDomainEventHandler : INotificationHandler<BookingReservedDomainEvent>
+{
+    private readonly IBookingRepository _bookingRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly IEmailService _emailService;
+
+    public BookingReservedDomainEventHandler(
+        IBookingRepository bookingRepository, 
+        IUserRepository userRepository, 
+        IEmailService emailService)
+    {
+        _bookingRepository = bookingRepository;
+        _userRepository = userRepository;
+        _emailService = emailService;
+    }
+
+    public async Task Handle(BookingReservedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var booking = await _bookingRepository.GetAsync(notification.BookingId, cancellationToken);
+
+        if (booking is null)
+        {
+            return;
+        }
+
+        var user = await _userRepository.GetAsync(booking.UserId, cancellationToken);
+
+        if (user is null)
+        {
+            return;
+        }
+
+        await _emailService.SendAsync(
+            user.Email,
+            subject: "Booking reserved!",
+            body: "You have 10 minutes to confirm this booking");
+    }
+}
