d10bc7eaa75fabebb39b84b5c178f9c731a02e9b
maucodev <hola@maucodev.com>
2026-01-29 19:07:48 -0600

    Solving logging as a cross-cutting concern

---
+++ Bookify.Application/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bookify.Application.Abstractions.Messaging;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Bookify.Application.Behaviors;
+
+public class LoggingBehavior<TRequest, TResponse> : 
+    IPipelineBehavior<TRequest, TResponse> 
+    where TRequest : IBaseCommand
+{
+    private readonly ILogger<TRequest> _logger;
+
+    public LoggingBehavior(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request, 
+        RequestHandlerDelegate<TResponse> next, 
+        CancellationToken cancellationToken)
+    {
+        var name = request.GetType().Name;
+
+        try
+        {
+            _logger.LogInformation("Executing command {Command}", name);
+
+            var result = await next();
+
+            _logger.LogInformation("Command {Command} executed successfully", name);
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Command {Command} executing failed", name);
+            throw;
+        }
+    }
+}
