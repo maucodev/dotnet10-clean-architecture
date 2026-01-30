432769866c94cf2f8bea12438c4419d29b6f993f
maucodev <hola@maucodev.com>
2026-01-29 20:27:34 -0600

    Creating the Domain entity configurations

---
+++ Booking.Infrastructure/Configurations/BookingConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Shared;
+using Bookify.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Booking.Infrastructure.Configurations;
+
+internal sealed class BookingConfiguration : IEntityTypeConfiguration<Bookify.Domain.Bookings.Booking>
+{
+    public void Configure(EntityTypeBuilder<Bookify.Domain.Bookings.Booking> builder)
+    {
+        builder.ToTable("bookings");
+
+        builder.HasKey(booking => booking.Id);
+
+        builder.OwnsOne(booking => booking.PriceForPeriod, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.CleaningFee, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.AmenitiesUpCharge, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.TotalPrice, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(booking => booking.Duration);
+
+        builder.HasOne<Apartment>()
+            .WithMany()
+            .HasForeignKey(booking => booking.ApartmentId);
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(booking => booking.UserId);
+    }
+}
