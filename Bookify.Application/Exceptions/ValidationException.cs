a5ed5dd4ab264ce2cfbe4d9264b7c944509a67dc
maucodev <hola@maucodev.com>
2026-01-29 19:26:16 -0600

    Creating a Validation pipeline for commands

---
+++ Bookify.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bookify.Application.Exceptions;
+
+public sealed class ValidationException : Exception
+{
+    public ValidationException(IEnumerable<ValidationError> errors)
+    {
+        Errors = errors;
+    }
+
+    public IEnumerable<ValidationError> Errors { get;  }
+}
