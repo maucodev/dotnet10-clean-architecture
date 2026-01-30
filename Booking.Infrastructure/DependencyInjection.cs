1f1498661b5537002306461e36e4aca84521df51
maucodev <hola@maucodev.com>
2026-01-29 19:52:31 -0600

    Configuring Entity Framework Core with dependency injection

--- Booking.Infrastructure/DependencyInjection.cs
+++ Booking.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,9 @@
-﻿using Bookify.Application.Abstractions.Clock;
+﻿using System;
+using Bookify.Application.Abstractions.Clock;
 using Bookify.Application.Abstractions.Email;
 using Booking.Infrastructure.Clock;
 using Booking.Infrastructure.Email;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,7 +16,19 @@ public static class DependencyInjection
         IConfiguration configuration)
     {
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+        
         services.AddTransient<IEmailService, EmailService>();
+
+        var connectionString =
+            configuration.GetConnectionString("Database") ??
+            throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options
+                .UseNpgsql(connectionString)
+                .UseSnakeCaseNamingConvention();
+        });
 
         return services;
     }
