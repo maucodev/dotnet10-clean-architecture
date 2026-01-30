b3dbae9f03652e23848fbddf95b8c90f3700872d
maucodev <hola@maucodev.com>
2026-01-29 19:42:52 -0600

    Configuring infrastructure services with dependency injection

---
+++ Booking.Infrastructure/Clock/DateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Bookify.Application.Abstractions.Clock;
+
+namespace Booking.Infrastructure.Clock;
+
+internal sealed class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
---
+++ Booking.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Bookify.Application.Abstractions.Email;
+
+namespace Booking.Infrastructure.Email;
+
+internal sealed class EmailService : IEmailService
+{
+    public Task SendAsync(Bookify.Domain.Users.Email recipient, string subject, string body)
+    {
+        return Task.CompletedTask;
+    }
+}
