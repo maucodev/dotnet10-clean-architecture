17783dc94a7be98de04ed19446f7fc101034b0f8
maucodev <hola@maucodev.com>
2026-01-29 19:48:07 -0600

    Adding Entity Framework Core to the solution

---
+++ Booking.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
